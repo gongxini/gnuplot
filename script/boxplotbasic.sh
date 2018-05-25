6e2ec59158d74a89166fd91cd6b341c0ed4a0c29
Yigong <yigongh@gmail.com>
2018-05-25 14:54:42 -0400

    syn

--- script/boxplotbasic.sh
+++ script/boxplotbasic.sh
@@ -8,21 +8,21 @@ else
 	gnuplot << EOF
 	set terminal postscript eps color solid linewidth 2 "Helvetica" 10
 	set output '$2/boxplot1.eps'
-	set border 2 front lt black linewidth 1.000 dashtype solid
+	set border 2 front  lt black linewidth 1.000 dashtype solid
 	set boxwidth 0.5 absolute
-	set style fill   solid 0.50 border lt -1
+	set style fill   solid 0.5 border lt -1
 	unset key
+	set grid
 	set pointsize 0.5
 	set style data boxplot
-	set xtics border in scale 0,0 nomirror norotate  autojustify
-	
+	set xtics border out scale 0,0 nomirror rotate  autojustify
+	set xtics norangelimit
 	set xtics   ("A" 1.00000, "B" 2.00000)
 	set ytics border in scale 1,0.5 nomirror norotate  autojustify
-	set yrange [ 0.00000 : 100.000 ] noreverse nowriteback
 	DEBUG_TERM_HTIC = 119
 	DEBUG_TERM_VTIC = 119
 	## Last datafile plotted: "silver.dat"
-	plot '$1' using (1):2, '' using (2):(5*\$3)
+	plot '$1' using (1):2, '' using (2):(\$3)
 quit
 EOF
 fi
